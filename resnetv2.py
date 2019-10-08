639758abd459d0bc6120504a19c4c4b2f4e2c8c9
hfercc <hfercxx@hotmail.com>
2019-10-09 01:50:38 +0800

    p

--- resnetv2.py
+++ resnetv2.py
@@ -47,6 +47,7 @@ class Bottleneck(nn.Module):
         self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
         self.bn0 = nn.BatchNorm2d(in_planes)
         self.shortcut = nn.Sequential()
+        self.relu = nn.ReLU(inplace = True)
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes, kernel_size=1, stride=stride, bias=False),
@@ -54,9 +55,9 @@ class Bottleneck(nn.Module):
             )
 
     def forward(self, x):
-        pre = F.relu(self.bn0(x))
-        out = F.relu(self.bn1(self.conv1(pre)))
-        out = F.relu(self.bn2(self.conv2(out)))
+        pre = self.relu(self.bn0(x))
+        out = self.relu(self.bn1(self.conv1(pre)))
+        out = self.relu(self.bn2(self.conv2(out)))
         out = self.conv3(out)
         if len(self.shortcut)==0:
             out += self.shortcut(x)
@@ -77,6 +78,8 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
         self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
         self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.relu = nn.ReLU(inplace = True)
+        self.avgpool2d = nn.AvgPool2d(7, stride = 1)
 
         def weights_init(m):
             if isinstance(m, nn.Conv2d):
@@ -96,15 +99,15 @@ class ResNet(nn.Module):
 
     def forward(self, x):
         #print(self.conv1(x))
-        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.relu(self.bn1(self.conv1(x)))
         out = self.layer1(out)
         out = self.layer2(out)
         out = self.layer3(out)
         #print(out.shape)
         out = self.layer4(out)
         
-        out = F.avg_pool2d(out, 8)
-        out = out.view(out.size(0), -1)
+        out = self.avg_pool2d(out, 8)
+        out = torch.flatten(out, 1)
         out = self.linear(out)
         #print(out)
         return out
