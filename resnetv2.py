5f546462b7e6ebaff67dfbb18f63b589fda8a56d
hfercc <hfercxx@hotmail.com>
2019-10-09 00:21:49 +0800

    p

--- resnetv2.py
+++ resnetv2.py
@@ -103,7 +103,7 @@ class ResNet(nn.Module):
         #print(out.shape)
         out = self.layer4(out)
         
-        out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         #print(out)
