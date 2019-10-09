56b1bdba0307c62653a4ec39b8b910a3cfae3deb
hfercc <hfercxx@hotmail.com>
2019-10-09 10:42:57 +0800

    p

--- resnetv2.py
+++ resnetv2.py
@@ -106,7 +106,7 @@ class ResNet(nn.Module):
         #print(out.shape)
         out = self.layer4(out)
         
-        out = self.avgpool2d(out, 8)
+        out = self.avgpool2d(out)
         out = torch.flatten(out, 1)
         out = self.linear(out)
         #print(out)
