06fec60337b346a592f7ae25c56bb3d0cfb410d5
hfercc <hfercxx@hotmail.com>
2019-10-11 23:13:05 +0800

    p

---
+++ ss.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+from torchvision.transforms.functional import rotate
+
+import numpy as np
+
+def rotation(input):
+    choice = np.random.randint(4)
+    batch = input.size[0]
+    target = torch.ones(batch, device = input.device) * choice
+    if choice == 0:
+        image = input
+    elif choice == 1:
+        image = rotate(input, 90)
+    elif choice == 2:
+        image = rotate(input, 180)
+    elif choice == 3:
+        image = rotate(input, 270)
+
+    return image, target
+
