afb1fceecd945e0054083ff2d34a3ba5999f429e
hfercc <hfercxx@hotmail.com>
2019-10-16 02:22:30 +0800

    p

---
+++ utils.py
@@ -0,0 +1,12 @@
+import torch
+
+def split_image(image, N):
+    """
+    image: (B, C, W, H)
+    """
+    batches = []
+
+    for i in list(torch.split(image, N, dim=2)):
+        batches.extend(list(torch.split(i, N, dim=3)))
+
+    return batches
