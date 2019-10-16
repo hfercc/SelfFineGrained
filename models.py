cca1a710cadcd823061cb4c395226967c07f8a05
hfercc <hfercxx@hotmail.com>
2019-10-16 18:21:55 +0800

    p

--- models.py
+++ models.py
@@ -29,7 +29,7 @@ class Model(nn.Module):
         if args.with_jigsaw:
             self.jigsaw_fc = nn.Sequential(
                 nn.Linear(2048 * 16, 2048),
-                nn.Linear(2048, 100))
+                nn.Linear(2048, 1000))
         else:
             self.jigsaw_fc = None
 
