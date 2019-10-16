d38469f1cdd73405a6e9e2e2cac02c6ecd2645bd
hfercc <hfercxx@hotmail.com>
2019-10-17 00:47:26 +0800

    p

--- train_jigsaw.py
+++ train_jigsaw.py
@@ -54,6 +54,10 @@ parser.add_argument('--with-jigsaw', action="store_true")
 parser.add_argument('--seperate-layer4', action="store_true")
 parser.add_argument('--rotation-aug', action="store_true")
 
+class fake:
+    def step():
+        pass
+
 def main():
     global args, best_prec1, summary_writer, jigsaw
 
@@ -121,6 +125,7 @@ def main():
 
     for epoch in range(args.start_epoch, args.epochs):
         trainObj, top1 = train(train_loader, model, criterion, optimizer, scheduler, epoch)
+        _,_ = train(val_loader, model, criterion, optimizer, fake, epoch)
         valObj, prec1 = val(val_loader, model, criterion)
         summary_writer.add_scalar("train_loss", trainObj, epoch)
         summary_writer.add_scalar("test_loss", valObj, epoch)
