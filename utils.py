7467f6c7866fdf9e7cef93d7a47f01cd6c70981c
hfercc <hfercxx@hotmail.com>
2019-10-20 01:01:01 +0800

    p

--- utils.py
+++ utils.py
@@ -14,6 +14,7 @@ def split_image(image, N):
 def combine_image(image, N):
 
     batches = torch.split(image, N * N, 1)
+    s = []
     for i in range(N):
-        batches.append(torch.cat(image[(i*N):((i + 1) * N - 1)], 3))
-    return torch.cat(batches, 2)
+        s.append(torch.cat(batches[(i*N):((i + 1) * N - 1)], 3))
+    return torch.cat(s, 2)
